2c16311a7a5145deae648bf41b5f6209d962b236
d <somejojostuf@gmail.com>
2020-01-26 11:37:55 +0200

    Added ADC and Timer

--- ShieldEkgEmgDemo/ShieldEkgEmgDemo.ino
+++ ShieldEkgEmgDemo/ShieldEkgEmgDemo.ino
@@ -56,6 +56,7 @@ struct Olimexino328_packet
 #define LED1  13
 #define CAL_SIG 9
 
+int pinADC=0;
 // Global constants and variables
 volatile unsigned char TXBuf[PACKETLEN];  //The transmission packet
 volatile unsigned char TXIndex;           //Next byte to write in the transmission packet.
@@ -96,106 +97,59 @@ void toggle_GAL_SIG(void){
  
 }
 
-
-/****************************************************/
-/*  Function name: setup                            */
-/*  Parameters                                      */
-/*    Input   :  No	                            */
-/*    Output  :  No                                 */
-/*    Action: Initializes all peripherals           */
-/****************************************************/
-void setup() {
-
+void setup() 
+{
  noInterrupts();  // Disable all interrupts before initialization
- 
- // LED1
  pinMode(LED1, OUTPUT);  //Setup LED1 direction
  digitalWrite(LED1,LOW); //Setup LED1 state
  pinMode(CAL_SIG, OUTPUT);
- 
- //Write packet header and footer
- TXBuf[0] = 0xa5;    //Sync 0
- TXBuf[1] = 0x5a;    //Sync 1
- TXBuf[2] = 2;       //Protocol version
- TXBuf[3] = 0;       //Packet counter
- TXBuf[4] = 0x02;    //CH1 High Byte
- TXBuf[5] = 0x00;    //CH1 Low Byte
- TXBuf[6] = 0x02;    //CH2 High Byte
- TXBuf[7] = 0x00;    //CH2 Low Byte
- TXBuf[8] = 0x02;    //CH3 High Byte
- TXBuf[9] = 0x00;    //CH3 Low Byte
- TXBuf[10] = 0x02;   //CH4 High Byte
- TXBuf[11] = 0x00;   //CH4 Low Byte
- TXBuf[12] = 0x02;   //CH5 High Byte
- TXBuf[13] = 0x00;   //CH5 Low Byte
- TXBuf[14] = 0x02;   //CH6 High Byte
- TXBuf[15] = 0x00;   //CH6 Low Byte 
- TXBuf[2 * NUMCHANNELS + HEADERLEN] =  0x01;	// Switches state
-
- // Timer2
- // Timer2 is used to setup the analag channels sampling frequency and packet update.
- // Whenever interrupt occures, the current read packet is sent to the PC
- // In addition the CAL_SIG is generated as well, so Timer1 is not required in this case!
- FlexiTimer2::set(TIMER2VAL, Timer2_Overflow_ISR);
- FlexiTimer2::start();
- 
- // Serial Port
  Serial.begin(115200);
- //Set speed to 57600 bps
- 
- // MCU sleep mode = idle.
- //outb(MCUCR,(inp(MCUCR) | (1<<SE)) & (~(1<<SM0) | ~(1<<SM1) | ~(1<<SM2)));
- 
+ adc_init();
+ timer1_init();
  interrupts();  // Enable all interrupts after initialization has been completed
 }
 
-/****************************************************/
-/*  Function name: Timer2_Overflow_ISR              */
-/*  Parameters                                      */
-/*    Input   :  No	                            */
-/*    Output  :  No                                 */
-/*    Action: Determines ADC sampling frequency.    */
-/****************************************************/
-void Timer2_Overflow_ISR()
-{
-  // Toggle LED1 with ADC sampling frequency /2
-  Toggle_LED1();
-  
-  //Read the 6 ADC inputs and store current values in Packet
-  for(CurrentCh=0;CurrentCh<NUMCHANNELS-1;CurrentCh++){
-    ADC_Value = analogRead(CurrentCh);
-    TXBuf[((2*CurrentCh) + HEADERLEN)] = ((unsigned char)((ADC_Value & 0xFF00) >> 8));	// Write High Byte
-    TXBuf[((2*CurrentCh) + HEADERLEN + 1)] = ((unsigned char)(ADC_Value & 0x00FF));	// Write Low Byte
-  }
-	 
-  // Send Packet
-  for(TXIndex=0;TXIndex<17;TXIndex++){
-    Serial.write(TXBuf[TXIndex]);
-  }
-    Serial.write(0x0d);
-    Serial.write(0x0a);
-    
-  // Increment the packet counter
-  TXBuf[3]++;			
-  
-  // Generate the CAL_SIGnal
-  counter++;		// increment the devider counter
-  if(counter == 12){	// 250/12/2 = 10.4Hz ->Toggle frequency
-    counter = 0;
-    toggle_GAL_SIG();	// Generate CAL signal with frequ ~10Hz
-  }
-}
 
 
-/****************************************************/
-/*  Function name: loop                             */
-/*  Parameters                                      */
-/*    Input   :  No	                            */
-/*    Output  :  No                                 */
-/*    Action: Puts MCU into sleep mode.             */
-/****************************************************/
 void loop() {
   
  __asm__ __volatile__ ("sleep");
  
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+   Serial.println(adc_read(pinADC));
+}
+
+void adc_init(){
+     
+    ADMUX = (1<<REFS0); //AVCC with external capacitor at AREF pin = 5V
+    ADCSRA = (1<<ADEN)|(1<<ADPS0);// ADC enable and system clock div 2
+    Serial.println("ADC_init");
+}
+
+static uint16_t adc_read(int pin)
+{
+  if (pin < 0 || pin > 5){ //ADC0-ADC5 is available
+    return 1; // pin number is out of range
+  }
+  pin &= 0x5;
+  ADMUX |= (ADMUX & 0xF8) |pin;
+  ADCSRA |= (1<<ADSC);
+  while (ADCSRA & (1<<ADSC)); //as long as ADSC pin is 1
+  return (ADC);
+}
+
+void timer1_init()
+{
+   TCCR1A = (1<<COM1A1);
+   TCCR1B = (0<<CS12)|(1<<CS11)|(0<<CS10)|(1<<WGM12); //Дільник = 8 (65535)
+
+   OCR1A = 2000; // 1 ms
+
+   TIMSK1 |= (1<<TOIE1)|(1<<OCIE1A);
+
+   Serial.println("Timer1_init");
+   sei();               
 }
